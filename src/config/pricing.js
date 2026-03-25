aca56d42b078514525e5ef01be9f095ddfefa0ab
Quentin Loader <46482883+QuentinLoader@users.noreply.github.com>
2026-03-25 14:24:05 +0200

    Pricing Update

--- src/config/pricing.js
+++ src/config/pricing.js
@@ -16,7 +16,7 @@ export const PRICING = {
       name: "Pay-as-YouScan",
       type: "credits",
       credits: 10,
-      price_cents: 29500,
+      price_cents: 2950,
       recurring: false
     },
 
